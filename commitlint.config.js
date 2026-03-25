2703a6ba6616d448cb7fe96a0bb12fe64b3647da
Francesco Del Re <francesco.delre.87@gmail.com>
2026-03-25 12:08:35 +0100

    ci(commitlint): update commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   parserPreset: 'conventional-changelog-conventionalcommits',
   rules: {
     'body-leading-blank': [1, 'always'],
@@ -18,61 +18,17 @@ module.exports = {
       type: {
         description: "Select the type of change that you're committing",
         enum: {
-          feat: {
-            description: 'A new feature',
-            title: 'Features',
-            emoji: '✨',
-          },
-          fix: {
-            description: 'A bug fix',
-            title: 'Bug Fixes',
-            emoji: '🐛',
-          },
-          docs: {
-            description: 'Documentation only changes',
-            title: 'Documentation',
-            emoji: '📚',
-          },
-          style: {
-            description: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-            title: 'Styles',
-            emoji: '💎',
-          },
-          refactor: {
-            description: 'A code change that neither fixes a bug nor adds a feature',
-            title: 'Code Refactoring',
-            emoji: '📦',
-          },
-          perf: {
-            description: 'A code change that improves performance',
-            title: 'Performance Improvements',
-            emoji: '🚀',
-          },
-          test: {
-            description: 'Adding missing tests or correcting existing tests',
-            title: 'Tests',
-            emoji: '🚨',
-          },
-          build: {
-            description: 'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
-            title: 'Builds',
-            emoji: '🛠',
-          },
-          ci: {
-            description: 'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
-            title: 'Continuous Integrations',
-            emoji: '⚙️',
-          },
-          chore: {
-            description: "Other changes that don't modify src or test files",
-            title: 'Chores',
-            emoji: '♻️',
-          },
-          revert: {
-            description: 'Reverts a previous commit',
-            title: 'Reverts',
-            emoji: '🗑',
-          },
+          feat: { description: 'A new feature', title: 'Features', emoji: '✨' },
+          fix: { description: 'A bug fix', title: 'Bug Fixes', emoji: '🐛' },
+          docs: { description: 'Documentation only changes', title: 'Documentation', emoji: '📚' },
+          style: { description: 'Changes that do not affect the meaning of the code', title: 'Styles', emoji: '💎' },
+          refactor: { description: 'A code change that neither fixes a bug nor adds a feature', title: 'Code Refactoring', emoji: '📦' },
+          perf: { description: 'A code change that improves performance', title: 'Performance Improvements', emoji: '🚀' },
+          test: { description: 'Adding missing tests or correcting existing tests', title: 'Tests', emoji: '🚨' },
+          build: { description: 'Changes that affect the build system', title: 'Builds', emoji: '🛠' },
+          ci: { description: 'Changes to CI configuration', title: 'Continuous Integrations', emoji: '⚙️' },
+          chore: { description: "Other changes that don't modify src or test files", title: 'Chores', emoji: '♻️' },
+          revert: { description: 'Reverts a previous commit', title: 'Reverts', emoji: '🗑' },
         },
       },
       scope: {
@@ -88,7 +44,7 @@ module.exports = {
         description: 'Are there any breaking changes?',
       },
       breakingBody: {
-        description: 'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself',
+        description: 'A BREAKING CHANGE commit requires a body.',
       },
       breaking: {
         description: 'Describe the breaking changes',
@@ -97,11 +53,11 @@ module.exports = {
         description: 'Does this change affect any open issues?',
       },
       issuesBody: {
-        description: 'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself',
+        description: 'If issues are closed, the commit requires a body.',
       },
       issues: {
-        description: 'Add issue references (e.g. "fix #123", "re #123".)',
+        description: 'Add issue references (e.g. "fix #123")',
       },
     },
   },
-}
+};
